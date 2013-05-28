5f0d10ceea029f64566c57e7fce592fa92c967d8
Eugen Cepoi <ecepoi@viadeoteam.com>
2013-05-28 14:40:18 +0200

    adding a builder method to QueryBuilder allowing to build a uri with the query string and base path, updated gitignore and added gitattributes to convert CRLF to LF

--- kasper-common/src/test/java/com/viadeo/kasper/query/exposition/QueryBuilderTest.java
+++ kasper-common/src/test/java/com/viadeo/kasper/query/exposition/QueryBuilderTest.java
@@ -9,6 +9,8 @@ package com.viadeo.kasper.query.exposition;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Map;
 
@@ -17,40 +19,40 @@ import static org.junit.Assert.assertTrue;
 
 public class QueryBuilderTest {
 
-	private QueryBuilder builder;
+    private QueryBuilder builder;
 
-	private static final String KEY = "key";
-	private static final String VALUE = "value";
-	private static final String ARRAY = "array";
-	
-	// ------------------------------------------------------------------------
-	
+    private static final String KEY = "key";
+    private static final String VALUE = "value";
+    private static final String ARRAY = "array";
+
+    // ------------------------------------------------------------------------
+
     @Before
     public void init() {
         builder = new QueryBuilder();
     }
 
-	// ------------------------------------------------------------------------
-    
+    // ------------------------------------------------------------------------
+
     @Test
     public void testSimpleValues() {
-    	// Given
-    	
-    	// When
+        // Given
+
+        // When
         builder.addSingle(KEY, VALUE);
-        
+
         // Then
         assertEquals(VALUE, builder.first(KEY));
     }
 
     @Test
     public void testMultipleValues() {
-    	
-    	// Given
-    	
-    	// When
+
+        // Given
+
+        // When
         builder.addSingle(KEY, VALUE).begin(ARRAY).add("1", "2").end();
-        
+
         // Then
         assertTrue(builder.hasSingle(KEY));
         assertEquals(VALUE, builder.first(KEY));
@@ -58,20 +60,20 @@ public class QueryBuilderTest {
         assertEquals("1", builder.values(ARRAY).get(0));
         assertEquals("2", builder.values(ARRAY).get(1));
     }
-    
-    @Test(expected=IllegalStateException.class)
+
+    @Test(expected = IllegalStateException.class)
     public void testBeginBeforeAdd() {
         builder.add("foo bar");
     }
-    
+
     @Test
     public void testBuild() {
-    	
-    	// Given
-    	
-    	// When
+
+        // Given
+
+        // When
         final Map<String, List<String>> map = builder.addSingle(KEY, VALUE).begin(ARRAY).add("1", "2").end().build();
-        
+
         // Then
         assertTrue(map.get(KEY).size() == 1);
         assertEquals(VALUE, map.get(KEY).get(0));
@@ -79,13 +81,15 @@ public class QueryBuilderTest {
         assertEquals("1", map.get(ARRAY).get(0));
         assertEquals("2", map.get(ARRAY).get(1));
     }
-    
-    @Test public void testComposedCallsToBegin() {
+
+    @Test
+    public void testComposedCallsToBegin() {
         // Given
-        
+
         // When
-        final Map<String, List<String>> map = builder.begin("14").add(1).begin("23").add(2).add(3).end().add(4).end().build();
-        
+        final Map<String, List<String>> map = builder.begin("14").add(1).begin("23").add(2).add(3).end().add(4).end()
+                .build();
+
         // Then
         assertEquals("1", map.get("14").get(0));
         assertEquals("4", map.get("14").get(1));
@@ -93,4 +97,11 @@ public class QueryBuilderTest {
         assertEquals("3", map.get("23").get(1));
     }
 
+    @Test
+    public void testBuildURI() throws URISyntaxException {
+        builder.addSingle("name", "fée").begin("names").add("foo", "bar").end();
+
+        assertEquals(new URI("http://www.google.com/somepath?name=f%C3%A9e&names=foo&names=bar").toASCIIString(),
+                builder.build(new URI("http://www.google.com/somepath")).toASCIIString());
+    }
 }
--- kasper-web/src/main/java/com/viadeo/kasper/exposition/HttpQueryExposer.java
+++ kasper-web/src/main/java/com/viadeo/kasper/exposition/HttpQueryExposer.java
@@ -36,186 +36,174 @@ import static javax.servlet.http.HttpServletResponse.SC_BAD_REQUEST;
 import static javax.servlet.http.HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
 
 public class HttpQueryExposer extends HttpExposer {
-	private static final long serialVersionUID = 8448984922303895624L;
+    private static final long serialVersionUID = 8448984922303895624L;
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(HttpQueryExposer.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(HttpQueryExposer.class);
 
-	private final Map<String, Class<? extends IQuery>> exposedQueries = Maps.newHashMap();
-	private IQueryServicesLocator queryServicesLocator;
-	private IQueryFactory queryAdapterFactory = new QueryFactoryBuilder().create();
+    private final Map<String, Class<? extends IQuery>> exposedQueries = Maps.newHashMap();
+    private IQueryServicesLocator queryServicesLocator;
+    private IQueryFactory queryAdapterFactory = new QueryFactoryBuilder().create();
 
-	public HttpQueryExposer(final IPlatform platform, final IQueryServicesLocator queryLocator) {
-	    super(platform);
-	    this.queryServicesLocator = queryLocator;
-	}
-	
-	@Override
-	public void init() throws ServletException {
-	    // expose all registered queries and commands
+    public HttpQueryExposer(final IPlatform platform, final IQueryServicesLocator queryLocator) {
+        super(platform);
+        this.queryServicesLocator = queryLocator;
+    }
+
+    @Override
+    public void init() throws ServletException {
+        // expose all registered queries and commands
         for (final IQueryService<? extends IQuery, ? extends IQueryDTO> queryService : queryServicesLocator
                 .getServices()) {
             expose(queryService);
         }
-	}
+    }
 
     // ------------------------------------------------------------------------
 
-	// again can not use sendError
-	@Override
-	protected void doGet(final HttpServletRequest req, final HttpServletResponse resp)
-            throws ServletException, IOException {
+    // again can not use sendError
+    @Override
+    protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException,
+            IOException {
 
-		// TODO we should think of providing some more information to client in
-		// case of failure. We also need to be sure that those infos a really
-		// useful
+        // TODO we should think of providing some more information to client in
+        // case of failure. We also need to be sure that those infos a really
+        // useful
 
-		// always respond with a json stream (even if empty)
-		resp.setContentType("application/json; charset=utf-8");
+        // always respond with a json stream (even if empty)
+        resp.setContentType("application/json; charset=utf-8");
 
-		/*
-		 * lets be very defensive and catch every thing in order to not break
-		 * the contract with clients = JSON only
-		 */
-		try {
-			final String queryName = resourceName(req.getRequestURI());
+        /*
+         * lets be very defensive and catch every thing in order to not break
+         * the contract with clients = JSON only
+         */
+        try {
+            final String queryName = resourceName(req.getRequestURI());
 
-			final IQuery query = parseQuery(queryName, req, resp);
+            final IQuery query = parseQuery(queryName, req, resp);
 
-			IQueryDTO dto = null;
-			if (!resp.isCommitted()) {
-				dto = handleQuery(queryName, query, resp);
-			}
+            IQueryDTO dto = null;
+            if (!resp.isCommitted()) {
+                dto = handleQuery(queryName, query, resp);
+            }
 
-			// need to check again as something might go wrong in handleQuery
-			if (!resp.isCommitted()) {
-				sendDTO(queryName, dto, resp);
-			}
+            // need to check again as something might go wrong in handleQuery
+            if (!resp.isCommitted()) {
+                sendDTO(queryName, dto, resp);
+            }
 
-		} catch (final Throwable t) {
-			sendError(SC_INTERNAL_SERVER_ERROR,
-					  "Could not handle query[" + req.getRequestURI()
-							+ "] and parameters[" + req.getQueryString() + "]",
-					  resp, t);
+        } catch (final Throwable t) {
+            sendError(SC_INTERNAL_SERVER_ERROR, "Could not handle query[" + req.getRequestURI() + "] and parameters["
+                    + req.getQueryString() + "]", resp, t);
 
-		}
+        }
 
-		resp.flushBuffer();
-	}
+        resp.flushBuffer();
+    }
 
     // ------------------------------------------------------------------------
 
-	// we can not use send error as it will send text/html response.
-	protected IQuery parseQuery(final String queryName,
-			                    final HttpServletRequest req,
-                                final HttpServletResponse resp)
-			throws IOException {
+    // we can not use send error as it will send text/html response.
+    protected IQuery parseQuery(final String queryName, final HttpServletRequest req, final HttpServletResponse resp)
+            throws IOException {
 
-		IQuery query = null;
-		final Class<? extends IQuery> queryClass = exposedQueries.get(queryName);
+        IQuery query = null;
+        final Class<? extends IQuery> queryClass = exposedQueries.get(queryName);
 
-		if (queryClass == null) {
+        if (queryClass == null) {
 
-			sendError(HttpServletResponse.SC_NOT_FOUND,
-                      "No such query[" + queryName + "].", resp, null);
+            sendError(HttpServletResponse.SC_NOT_FOUND, "No such query[" + queryName + "].", resp, null);
 
         } else {
 
-			final ITypeAdapter<? extends IQuery> adapter =
-                    queryAdapterFactory.create(TypeToken.of(queryClass));
+            final ITypeAdapter<? extends IQuery> adapter = queryAdapterFactory.create(TypeToken.of(queryClass));
 
-			final Map<String, List<String>> queryParams = new HashMap<>();
+            final Map<String, List<String>> queryParams = new HashMap<>();
 
-            final Enumeration keys = req.getParameterNames();
-            while(keys.hasMoreElements()) {
-				final String key = (String) keys.nextElement();
-				queryParams.put(key, Arrays.asList(req.getParameterValues(key)));
-			}
+            final Enumeration<String> keys = req.getParameterNames();
+            while (keys.hasMoreElements()) {
+                final String key = (String) keys.nextElement();
+                queryParams.put(key, Arrays.asList(req.getParameterValues(key)));
+            }
 
-			try {
+            try {
 
-				query = adapter.adapt(new QueryParser(queryParams));
+                query = adapter.adapt(new QueryParser(queryParams));
 
-			} catch (final Throwable t) {
-				/*
-				 * OK lets catch any exception that could occur during
-				 * deserialization and try to send back
-				 */
-				sendError(SC_BAD_REQUEST,
-						"Unable to parse Query[" + queryName
-								+ "] and parameters [" + req.getQueryString()
-								+ "].", resp, t);
-			}
-		}
+            } catch (final Throwable t) {
+                /*
+                 * OK lets catch any exception that could occur during
+                 * deserialization and try to send back
+                 */
+                sendError(SC_BAD_REQUEST,
+                        "Unable to parse Query[" + queryName + "] and parameters [" + req.getQueryString() + "].",
+                        resp, t);
+            }
+        }
 
-		return query;
-	}
+        return query;
+    }
 
     // ------------------------------------------------------------------------
 
-	// can not use sendError it is forcing response to text/html
-	protected IQueryDTO handleQuery(final String queryName, final IQuery query,	final HttpServletResponse resp)
+    // can not use sendError it is forcing response to text/html
+    protected IQueryDTO handleQuery(final String queryName, final IQuery query, final HttpServletResponse resp)
             throws IOException {
 
         IQueryDTO dto = null;
 
         try {
 
-			/*
-			 * checking if the response has not been sent, if it is true this
-			 * means that an error happened and has been handled
-			 */
-			dto = platform().getQueryGateway().retrieve(
-					new DefaultContextBuilder().buildDefault(), query);
+            /*
+             * checking if the response has not been sent, if it is true this
+             * means that an error happened and has been handled
+             */
+            dto = platform().getQueryGateway().retrieve(new DefaultContextBuilder().buildDefault(), query);
 
-		} catch (final Throwable e) {
-			/*
-			 * it is ok to eat all kind of exceptions as they occur at parsing
-			 * level so we know what approximatively failed.
-			 */
-			sendError(SC_INTERNAL_SERVER_ERROR,
-					  "ERROR Submiting query[" + queryName
-							+ "] to Kasper platform.", resp, e);
-		}
-
-		return dto;
-	}
-
-    // ------------------------------------------------------------------------
-
-	// can not use sendError it is forcing response to text/html
-	protected void sendDTO(final String queryName, final IQueryDTO dto, final HttpServletResponse resp)
-            throws IOException {
-
-		final ObjectWriter writer = ObjectMapperProvider.instance.objectWriter();
-
-		try {
-
-			writer.writeValue(resp.getOutputStream(), dto);
-			resp.setStatus(HttpServletResponse.SC_OK);
-
-		} catch (final Throwable t) {
-			sendError(SC_INTERNAL_SERVER_ERROR,
-					"ERROR sending DTO[" + dto.getClass().getSimpleName()
-							+ "] for query[" + queryName + "].", resp, t);
-		}
-	}
-
-    // ------------------------------------------------------------------------
-
-	@SuppressWarnings("deprecation")
-	private void sendError(final int status, final String message,
-			               final HttpServletResponse resp, final Throwable exception)
-            throws IOException {
-
-		if (exception != null) {
-			LOGGER.error(message, exception);
-        } else {
-			LOGGER.error(message);
+        } catch (final Throwable e) {
+            /*
+             * it is ok to eat all kind of exceptions as they occur at parsing
+             * level so we know what approximatively failed.
+             */
+            sendError(SC_INTERNAL_SERVER_ERROR, "ERROR Submiting query[" + queryName + "] to Kasper platform.", resp, e);
         }
 
-		resp.setStatus(status, message);
+        return dto;
+    }
 
-		final ObjectWriter writer = ObjectMapperProvider.instance.objectWriter();
+    // ------------------------------------------------------------------------
+
+    // can not use sendError it is forcing response to text/html
+    protected void sendDTO(final String queryName, final IQueryDTO dto, final HttpServletResponse resp)
+            throws IOException {
+
+        final ObjectWriter writer = ObjectMapperProvider.instance.objectWriter();
+
+        try {
+
+            writer.writeValue(resp.getOutputStream(), dto);
+            resp.setStatus(HttpServletResponse.SC_OK);
+
+        } catch (final Throwable t) {
+            sendError(SC_INTERNAL_SERVER_ERROR, "ERROR sending DTO[" + dto.getClass().getSimpleName() + "] for query["
+                    + queryName + "].", resp, t);
+        }
+    }
+
+    // ------------------------------------------------------------------------
+
+    @SuppressWarnings("deprecation")
+    private void sendError(final int status, final String message, final HttpServletResponse resp,
+            final Throwable exception) throws IOException {
+
+        if (exception != null) {
+            LOGGER.error(message, exception);
+        } else {
+            LOGGER.error(message);
+        }
+
+        resp.setStatus(status, message);
+
+        final ObjectWriter writer = ObjectMapperProvider.instance.objectWriter();
 
         try (final JsonGenerator generator = writer.getJsonFactory().createGenerator(resp.getOutputStream())) {
 
@@ -227,30 +215,28 @@ public class HttpQueryExposer extends HttpExposer {
 
         }
 
-		resp.flushBuffer();
-	}
+        resp.flushBuffer();
+    }
 
     // ------------------------------------------------------------------------
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	HttpQueryExposer expose(final IQueryService<? extends IQuery, ? extends IQueryDTO> queryService) {
-		checkNotNull(queryService);
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    HttpQueryExposer expose(final IQueryService<? extends IQuery, ? extends IQueryDTO> queryService) {
+        checkNotNull(queryService);
 
-		final TypeToken<? extends IQueryService> typeToken = TypeToken.of(queryService.getClass());
-		final Class<? super IQuery> queryClass = (Class<? super IQuery>) typeToken
-				.getSupertype(IQueryService.class)
-				.resolveType(IQueryService.class.getTypeParameters()[0])
-				.getRawType();
+        final TypeToken<? extends IQueryService> typeToken = TypeToken.of(queryService.getClass());
+        final Class<? super IQuery> queryClass = (Class<? super IQuery>) typeToken.getSupertype(IQueryService.class)
+                .resolveType(IQueryService.class.getTypeParameters()[0]).getRawType();
 
-		putKey(queryToPath(queryClass), queryClass, exposedQueries);
+        putKey(queryToPath(queryClass), queryClass, exposedQueries);
 
         return this;
-	}
+    }
 
     // ------------------------------------------------------------------------
 
-	private String queryToPath(final Class<? super IQuery> exposedQuery) {
-		return Introspector.decapitalize(exposedQuery.getSimpleName().replaceAll("Query", ""));
-	}
+    private String queryToPath(final Class<? super IQuery> exposedQuery) {
+        return Introspector.decapitalize(exposedQuery.getSimpleName().replaceAll("Query", ""));
+    }
 
 }
