2301ca5f2db68c07b58556c65c16908eb2fef2e7
Christian Nguyen Van Than <christian.nguyenvanthan@gmail.com>
2013-06-03 12:01:57 +0200

    Merged nguyenvanthan/ec28f06 - fix sql parsing

--- kasper-datasource/src/main/java/com/viadeo/kasper/db/jdbc/SQLParser.java
+++ kasper-datasource/src/main/java/com/viadeo/kasper/db/jdbc/SQLParser.java
@@ -7,6 +7,7 @@
 package com.viadeo.kasper.db.jdbc;
 
 import com.google.common.base.Preconditions;
+import com.google.common.base.Splitter;
 import com.viadeo.kasper.db.Operation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +16,8 @@ import java.util.Scanner;
 
 public class SQLParser {
     private static final Logger LOGGER = LoggerFactory.getLogger(SQLParser.class);
+
+    private static final String EMPTY = "";
 
     public static SQLQuery parse(final String sql) {
         Preconditions.checkNotNull(sql); // fast fail
@@ -32,7 +35,7 @@ public class SQLParser {
             }
             scanner.next(); // go throught the next statement
 
-            final String table = scanner.useDelimiter(",").next();
+            final String table = keepSingleValue(scanner.next()); // contains tables names
             query.setTableName(table.trim());
 
         } else if ("INSERT".equalsIgnoreCase(op)){
@@ -44,7 +47,7 @@ public class SQLParser {
             }
             scanner.next(); // go throught the next statement
 
-            final String table = scanner.useDelimiter("[,\\(]").next();
+            final String table = scanner.useDelimiter("[,\\(\\s]").next();
             query.setTableName(table.trim());
 
         } else if ("UPDATE".equalsIgnoreCase(op)) {
@@ -68,6 +71,30 @@ public class SQLParser {
 
         scanner.close();
         return query;
+    }
+
+    /**
+     * Split to several values if the separator comma character(',') is present but keep only the first value
+     * @param multivalue String containing several value separated by a comma
+     * @return return the first value or an empty String ("") if argument {@code multivalue} is null
+     */
+     static String keepSingleValue(final String multivalue) {
+        String result = EMPTY;
+
+        // fast return
+        if (null == multivalue) {
+            return result;
+        }
+
+        // filtering
+        int indexOfComma = multivalue.indexOf(",");
+        if ( -1 == indexOfComma ) { // if no any comma
+            result = multivalue;
+        } else {
+            result = multivalue.substring(0, indexOfComma).trim();
+        }
+
+        return result;
     }
 
 }
