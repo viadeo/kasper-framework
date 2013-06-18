6f6d6f3cc90e55de0f541f3d71e936121a15a5ec
Christian Nguyen Van Than <christian.nguyenvanthan@gmail.com>
2013-06-18 14:10:43 +0200

    add utility method and deprecated ArrayWrapper

--- kasper-datasource/src/main/java/com/viadeo/kasper/shard/util/Sql.java
+++ kasper-datasource/src/main/java/com/viadeo/kasper/shard/util/Sql.java
@@ -8,9 +8,18 @@ package com.viadeo.kasper.shard.util;
 
 import com.google.common.base.Joiner;
 
+import java.util.Collection;
+
 public class Sql {
 
     public static String in(final Object... items) {
+        return Joiner.on(",")
+                .skipNulls()
+                .join(items)
+                .replaceAll(" ","");
+    }
+
+    public static String in(final Collection<?> items) {
         return Joiner.on(",")
                 .skipNulls()
                 .join(items)
