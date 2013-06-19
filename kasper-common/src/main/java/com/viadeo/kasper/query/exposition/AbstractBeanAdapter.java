3cbdebd6e360c9538422fb1673939faf01f02b23
Loïc Dias Da Silva <ldiasdasilva@viadeoteam.com>
2013-06-19 19:44:27 +0200

    missing header

--- kasper-common/src/main/java/com/viadeo/kasper/query/exposition/AbstractBeanAdapter.java
+++ kasper-common/src/main/java/com/viadeo/kasper/query/exposition/AbstractBeanAdapter.java
@@ -1,3 +1,9 @@
+// ============================================================================
+//                 KASPER - Kasper is the treasure keeper
+//    www.viadeo.com - mobile.viadeo.com - api.viadeo.com - dev.viadeo.com
+//
+//           Viadeo Framework for effective CQRS/DDD architecture
+// ============================================================================
 package com.viadeo.kasper.query.exposition;
 
 /* I am not sure of the name, bean adapter sounds good but this class
