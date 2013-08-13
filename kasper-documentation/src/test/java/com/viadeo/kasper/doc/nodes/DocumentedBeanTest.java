5546a8803b0f10afdbaa3fef06e2ad797fa83c2b
Loïc Dias Da Silva <ldiasdasilva@viadeoteam.com>
2013-08-13 02:08:22 +0200

    feature: ReflectionGenericsResolver can now deal with generic fields

---
+++ kasper-documentation/src/test/java/com/viadeo/kasper/doc/nodes/DocumentedBeanTest.java
@@ -0,0 +1,11 @@
+package com.viadeo.kasper.doc.nodes;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mglcel
+ * Date: 8/13/13
+ * Time: 12:51 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DocumentedBeanTest {
+}
