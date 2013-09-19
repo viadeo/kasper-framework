a25816c43f717a73b35602f62c09bee8ba5f8155
Zeyu LIN <zlin@viadeoteam.com>
2013-09-19 15:21:39 +0200

    improve Web UI: add hearder-query, hearder-queryPayload, header-queryService

--- kasper-documentation/src/main/java/com/viadeo/kasper/doc/nodes/DocumentedMethod.java
+++ kasper-documentation/src/main/java/com/viadeo/kasper/doc/nodes/DocumentedMethod.java
@@ -13,13 +13,15 @@ public class DocumentedMethod {
     private final String name;
     private final List<Map<String,String>> parameters;
     private final String returnType;
+    private final String exceptionType;
 
     // -------------------------------------------------------------
 
-    public DocumentedMethod(String name, List<Map<String, String>> parameters, String returnType) {
+    public DocumentedMethod(String name, List<Map<String, String>> parameters, String returnType, String exceptionType) {
         this.name = name;
         this.parameters = parameters;
         this.returnType = returnType;
+        this.exceptionType =exceptionType;
     }
 
     // -------------------------------------------------------------
@@ -29,8 +31,13 @@ public class DocumentedMethod {
         return name;
     }
 
+    public String getExceptionType() {
+        return exceptionType;
+    }
+
     public List<Map<String, String>> getParameters() {
         return parameters;
+
     }
 
     public String getReturnType() {
