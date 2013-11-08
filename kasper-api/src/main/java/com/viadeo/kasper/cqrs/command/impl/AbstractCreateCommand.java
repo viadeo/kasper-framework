fb1cbdb302933d4b7dd1a416a170883825b58903
Loïc Dias Da Silva <mglcel@mglcel.fr>
2013-11-08 20:30:08 +0100

    bugfix: added equals/hashCode/toString to AbstractCreateCommand

--- kasper-api/src/main/java/com/viadeo/kasper/cqrs/command/impl/AbstractCreateCommand.java
+++ kasper-api/src/main/java/com/viadeo/kasper/cqrs/command/impl/AbstractCreateCommand.java
@@ -47,4 +47,31 @@ public abstract class AbstractCreateCommand implements CreateCommand {
 		return Optional.fromNullable(this.idToUse);
 	}
 
+    // ------------------------------------------------------------------------
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final AbstractCreateCommand other = (AbstractCreateCommand) obj;
+
+        return com.google.common.base.Objects.equal(this.idToUse, other.idToUse);
+    }
+
+    @Override
+    public int hashCode() {
+        return com.google.common.base.Objects.hashCode(this.idToUse);
+    }
+
+    @Override
+    public String toString() {
+        return com.google.common.base.Objects.toStringHelper(this)
+                .addValue(this.idToUse)
+                .toString();
+    }
+
 }
