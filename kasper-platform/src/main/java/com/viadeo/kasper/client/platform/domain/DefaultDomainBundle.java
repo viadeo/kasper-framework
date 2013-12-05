dd02c744d376ef65bb05df33e9ce3e17cb8c21ae
cmurer <cmurer@viadeoteam.com>
2013-12-05 15:19:57 +0100

    Added new constructor into DefaultDomainBundle

--- kasper-platform/src/main/java/com/viadeo/kasper/client/platform/domain/DefaultDomainBundle.java
+++ kasper-platform/src/main/java/com/viadeo/kasper/client/platform/domain/DefaultDomainBundle.java
@@ -2,6 +2,7 @@ package com.viadeo.kasper.client.platform.domain;
 
 import com.google.common.collect.Lists;
 import com.viadeo.kasper.client.platform.Platform;
+import com.viadeo.kasper.core.resolvers.DomainResolver;
 import com.viadeo.kasper.cqrs.command.CommandHandler;
 import com.viadeo.kasper.cqrs.query.QueryHandler;
 import com.viadeo.kasper.ddd.Domain;
@@ -19,13 +20,8 @@ public class DefaultDomainBundle implements DomainBundle {
     protected final Domain domain;
     protected final String name;
 
-    public DefaultDomainBundle(List<CommandHandler> commandHandlers, List<QueryHandler> queryHandlers, List<Repository> repositories, List<EventListener> eventListeners, Domain domain, String name) {
-        this.commandHandlers = commandHandlers;
-        this.queryHandlers = queryHandlers;
-        this.repositories = repositories;
-        this.eventListeners = eventListeners;
-        this.domain = domain;
-        this.name = name;
+    public DefaultDomainBundle(Domain domain) {
+        this(domain, new DomainResolver().getLabel(domain.getClass()));
     }
 
     public DefaultDomainBundle(Domain domain, String name) {
@@ -36,6 +32,15 @@ public class DefaultDomainBundle implements DomainBundle {
                 , domain
                 , name
         );
+    }
+
+    public DefaultDomainBundle(List<CommandHandler> commandHandlers, List<QueryHandler> queryHandlers, List<Repository> repositories, List<EventListener> eventListeners, Domain domain, String name) {
+        this.commandHandlers = commandHandlers;
+        this.queryHandlers = queryHandlers;
+        this.repositories = repositories;
+        this.eventListeners = eventListeners;
+        this.domain = domain;
+        this.name = name;
     }
 
     @Override
