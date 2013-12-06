617a3e404b7c29379439b784a21768d34453e5b8
cmurer <cmurer@viadeoteam.com>
2013-12-06 12:45:20 +0100

    Return a KasperDocResource Instead of DocumentedPlatform

--- kasper-documentation/src/main/java/com/viadeo/kasper/doc/DocumentationPlugin.java
+++ kasper-documentation/src/main/java/com/viadeo/kasper/doc/DocumentationPlugin.java
@@ -1,30 +1,46 @@
 package com.viadeo.kasper.doc;
 
+import com.google.common.base.Preconditions;
 import com.viadeo.kasper.client.platform.Platform;
 import com.viadeo.kasper.client.platform.Plugin;
 import com.viadeo.kasper.client.platform.domain.descriptor.DomainDescriptor;
 import com.viadeo.kasper.doc.element.DocumentedPlatform;
 import com.viadeo.kasper.doc.initializer.DefaultDocumentedElementInitializer;
-
+import com.viadeo.kasper.doc.web.KasperDocResource;
 
 public class DocumentationPlugin implements Plugin {
 
     private final DocumentedPlatform documentedPlatform;
+    private boolean initialized;
 
     public DocumentationPlugin() {
-        this.documentedPlatform = new DocumentedPlatform();
+        this(new DocumentedPlatform());
+
+    }
+
+    protected DocumentationPlugin(DocumentedPlatform documentedPlatform) {
+        this.documentedPlatform = documentedPlatform;
+        this.initialized = false;
     }
 
     @Override
     public void initialize(Platform platform, DomainDescriptor... domainDescriptors) {
+        Preconditions.checkNotNull(domainDescriptors);
+
         for (DomainDescriptor domainDescriptor : domainDescriptors) {
             documentedPlatform.registerDomain(domainDescriptor.getName(), domainDescriptor);
         }
 
         documentedPlatform.accept(new DefaultDocumentedElementInitializer());
+        initialized = true;
     }
 
-    public DocumentedPlatform getDocumentedPlatform() {
-        return documentedPlatform;
+    public KasperDocResource getKasperDocResource() {
+        Preconditions.checkState(initialized, "The documentation plugin must be initialized");
+        return new KasperDocResource(documentedPlatform);
+    }
+
+    protected boolean isInitialized() {
+        return initialized;
     }
 }
---
+++ kasper-documentation/src/test/java/com/viadeo/kasper/doc/DocumentationPluginUTest.java
@@ -0,0 +1,98 @@
+package com.viadeo.kasper.doc;
+
+import com.viadeo.kasper.client.platform.Platform;
+import com.viadeo.kasper.client.platform.domain.descriptor.DomainDescriptor;
+import com.viadeo.kasper.doc.element.DocumentedPlatform;
+import com.viadeo.kasper.doc.initializer.DocumentedElementVisitor;
+import com.viadeo.kasper.doc.web.KasperDocResource;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.*;
+
+public class DocumentationPluginUTest {
+
+    private DocumentedPlatform documentedPlatform;
+
+    public DocumentationPluginUTest(){
+        documentedPlatform = mock(DocumentedPlatform.class);
+    }
+
+    @Before
+    public void setUp(){
+        reset(documentedPlatform);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void getKasperDocResource_fromNonInitializedPlugin_shouldThrowException(){
+        // Given
+        DocumentationPlugin documentationPlugin = new DocumentationPlugin(documentedPlatform);
+
+        // When
+        documentationPlugin.getKasperDocResource();
+
+        // Then throws an exception
+    }
+
+    @Test
+    public void getKasperDocResource_fromInitializedPlugin_shouldReturnDocumentation(){
+        // Given
+        DocumentationPlugin documentationPlugin = new DocumentationPlugin(documentedPlatform);
+        documentationPlugin.initialize(mock(Platform.class));
+
+        // When
+        KasperDocResource kasperDocResource = documentationPlugin.getKasperDocResource();
+
+        // Then
+        Assert.assertTrue(documentationPlugin.isInitialized());
+        Assert.assertNotNull(kasperDocResource);
+    }
+
+    @Test
+    public void initialize_withNoDomainBundle_shouldBeOk(){
+        // Given
+        DocumentationPlugin documentationPlugin = new DocumentationPlugin(documentedPlatform);
+        DomainDescriptor[] descriptors = {};
+
+        // When
+        documentationPlugin.initialize(mock(Platform.class), descriptors);
+
+        // Then
+        Assert.assertTrue(documentationPlugin.isInitialized());
+        verify(documentedPlatform).accept(any(DocumentedElementVisitor.class));
+        verifyNoMoreInteractions(documentedPlatform);
+    }
+
+    @Test
+    public void initialize_withDomainBundle_shouldBeOk(){
+        // Given
+        DocumentationPlugin documentationPlugin = new DocumentationPlugin(documentedPlatform);
+
+        String domainName = "Foobar";
+
+        DomainDescriptor descriptor = mock(DomainDescriptor.class);
+        when(descriptor.getName()).thenReturn(domainName);
+        DomainDescriptor[] descriptors = {descriptor};
+
+        // When
+        documentationPlugin.initialize(mock(Platform.class), descriptors);
+
+        // Then
+        Assert.assertTrue(documentationPlugin.isInitialized());
+        verify(documentedPlatform).registerDomain(refEq(domainName), refEq(descriptor));
+        verify(documentedPlatform).accept(any(DocumentedElementVisitor.class));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void initialize_withNullAsDomainDescriptors_shouldThrowException(){
+        // Given
+        DocumentationPlugin documentationPlugin = new DocumentationPlugin(documentedPlatform);
+        DomainDescriptor[] descriptors = null;
+
+        // When
+        documentationPlugin.initialize(mock(Platform.class), descriptors);
+
+        // Then throws an exception
+    }
+}
