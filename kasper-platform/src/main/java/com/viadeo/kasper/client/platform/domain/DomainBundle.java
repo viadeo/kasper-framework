7e1c68bab8d06071c2cf2e0863d3983462ac584f
cmurer <cmurer@viadeoteam.com>
2013-12-09 11:45:04 +0100

    Added Builder of DomainBundle

--- kasper-platform/src/main/java/com/viadeo/kasper/client/platform/domain/DomainBundle.java
+++ kasper-platform/src/main/java/com/viadeo/kasper/client/platform/domain/DomainBundle.java
@@ -1,5 +1,8 @@
 package com.viadeo.kasper.client.platform.domain;
 
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
+import com.viadeo.kasper.core.resolvers.DomainResolver;
 import com.viadeo.kasper.cqrs.command.CommandHandler;
 import com.viadeo.kasper.cqrs.query.QueryHandler;
 import com.viadeo.kasper.ddd.Domain;
@@ -11,6 +14,7 @@ import java.util.List;
 import static com.viadeo.kasper.client.platform.Platform.BuilderContext;
 
 public interface DomainBundle {
+
     void configure(BuilderContext context);
 
     String getName();
@@ -24,4 +28,55 @@ public interface DomainBundle {
     List<EventListener> getEventListeners();
 
     List<Repository> getRepositories();
+
+
+    public static class Builder {
+
+        private final Domain domain;
+        private final String domainName;
+        private final List<CommandHandler> commandHandlers;
+        private final List<QueryHandler> queryHandlers;
+        private final List<EventListener> eventListeners;
+        private final List<Repository> repositories;
+
+        public Builder(Domain domain){
+            this.domain = Preconditions.checkNotNull(domain);
+            this.domainName = new DomainResolver().getLabel(domain.getClass());
+            this.commandHandlers = Lists.newArrayList();
+            this.queryHandlers = Lists.newArrayList();
+            this.eventListeners = Lists.newArrayList();
+            this.repositories = Lists.newArrayList();
+        }
+
+        public Builder with(CommandHandler commandHandler){
+            commandHandlers.add(Preconditions.checkNotNull(commandHandler));
+            return this;
+        }
+
+        public Builder with(QueryHandler queryHandler){
+            queryHandlers.add(Preconditions.checkNotNull(queryHandler));
+            return this;
+        }
+
+        public Builder with(EventListener eventListener){
+            eventListeners.add(Preconditions.checkNotNull(eventListener));
+            return this;
+        }
+
+        public Builder with(Repository repository){
+            repositories.add(Preconditions.checkNotNull(repository));
+            return this;
+        }
+
+        public DomainBundle build(){
+            return new DefaultDomainBundle(
+                    commandHandlers
+                    , queryHandlers
+                    , repositories
+                    , eventListeners
+                    , domain
+                    , domainName
+            );
+        }
+    }
 }
