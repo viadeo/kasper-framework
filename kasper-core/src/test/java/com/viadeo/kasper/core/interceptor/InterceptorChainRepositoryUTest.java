e19735a99125aadbcecfe20197d2db7a1b3be1ca
cmurer <cmurer@viadeoteam.com>
2014-01-02 15:14:24 +0100

    Fix bug around chain creation when we have minimum two elements contained in the chain

--- kasper-core/src/test/java/com/viadeo/kasper/core/interceptor/InterceptorChainRepositoryUTest.java
+++ kasper-core/src/test/java/com/viadeo/kasper/core/interceptor/InterceptorChainRepositoryUTest.java
@@ -54,7 +54,7 @@ public class InterceptorChainRepositoryUTest {
     public void get_afterCreate_shouldReturnChain() {
         // Given
         Class key = String.class;
-        Interceptor<Object, Object> tail = mock(Interceptor.class);
+        Interceptor<Object, Object> tail = createMockedInterceptor("tail");
         chainRepository.create(key, tail);
 
         // When
@@ -71,7 +71,7 @@ public class InterceptorChainRepositoryUTest {
     public void create_withoutRegisteredInterceptorFactory_shouldCreateChain() {
         // Given
         Class key = String.class;
-        Interceptor<Object, Object> tail = mock(Interceptor.class);
+        Interceptor<Object, Object> tail = createMockedInterceptor("tail");
 
         // When
         Optional<InterceptorChain<Object, Object>> chainOptional = chainRepository.create(key, tail);
@@ -87,8 +87,8 @@ public class InterceptorChainRepositoryUTest {
         // Given
         Class key = String.class;
 
-        Interceptor<Object, Object> head = mock(Interceptor.class);
-        Interceptor<Object, Object> tail = mock(Interceptor.class);
+        Interceptor<Object, Object> head = createMockedInterceptor("head");
+        Interceptor<Object, Object> tail = createMockedInterceptor("tail");
 
         InterceptorFactory<Object, Object> headFactory = mock(InterceptorFactory.class);
         when(headFactory.create(any(TypeToken.class))).thenReturn(Optional.of(InterceptorChain.makeChain(head)));
@@ -104,5 +104,60 @@ public class InterceptorChainRepositoryUTest {
 
         assertEquals(head, chainOptional.get().actor.get());
         assertEquals(tail, chainOptional.get().next.get().actor.get());
+    }
+
+    @Test
+    public void create_withMinimumTwoRegisteredInterceptorFactory_shouldCreateChain() {
+        // Given
+        Class key = String.class;
+
+        Interceptor<Object, Object> elem1 = createMockedInterceptor("elem1");
+        Interceptor<Object, Object> elem2 = createMockedInterceptor("elem2");
+        Interceptor<Object, Object> tail = createMockedInterceptor("tail");
+
+        chainRepository.register(new MockInterceptorFactory(elem1));
+        chainRepository.register(new MockInterceptorFactory(elem2));
+
+        // When
+        Optional<InterceptorChain<Object, Object>> chainOptional = chainRepository.create(key, tail);
+
+        // Then
+        assertNotNull(chainOptional);
+        assertTrue(chainOptional.isPresent());
+
+        InterceptorChain<Object, Object> chain1 = chainOptional.get();
+        assertEquals(elem1, chain1.actor.get());
+
+        InterceptorChain<Object, Object> chain2 = chain1.next.get();
+        assertEquals(elem2, chain2.actor.get());
+
+        InterceptorChain<Object, Object> chain3 = chain2.next.get();
+        assertEquals(tail, chain3.actor.get());
+        assertEquals(InterceptorChain.TAIL, chain3.next.get());
+    }
+
+    @SuppressWarnings("unchecked")
+    private static Interceptor<Object, Object> createMockedInterceptor(final String name) {
+        Interceptor<Object, Object> interceptor = mock(Interceptor.class);
+        when(interceptor.toString()).thenReturn(name);
+        return interceptor;
+    }
+
+    private static class MockInterceptorFactory implements InterceptorFactory<Object, Object> {
+        private final InterceptorChain<Object, Object> interceptorChain;
+
+        public MockInterceptorFactory(Interceptor<Object, Object> interceptor) {
+            this.interceptorChain = InterceptorChain.makeChain(interceptor);
+        }
+
+        @Override
+        public Optional<InterceptorChain<Object, Object>> create(TypeToken<?> type) {
+            return Optional.of(interceptorChain);
+        }
+
+        @Override
+        public boolean accept(TypeToken<?> type) {
+            return true;
+        }
     }
 }
