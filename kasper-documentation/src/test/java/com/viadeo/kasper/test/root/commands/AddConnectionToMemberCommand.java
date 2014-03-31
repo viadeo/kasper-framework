789fcadf52dca58ae5a0de4f76e0c17d4c606bf0
cmurer <cmurer@viadeoteam.com>
2014-03-31 14:33:53 +0200

    improve component-card template

--- kasper-documentation/src/test/java/com/viadeo/kasper/test/root/commands/AddConnectionToMemberCommand.java
+++ kasper-documentation/src/test/java/com/viadeo/kasper/test/root/commands/AddConnectionToMemberCommand.java
@@ -3,8 +3,15 @@ package com.viadeo.kasper.test.root.commands;
 import com.viadeo.kasper.cqrs.command.Command;
 import com.viadeo.kasper.cqrs.command.annotation.XKasperCommand;
 
+import javax.validation.constraints.NotNull;
+
 @XKasperCommand
 public class AddConnectionToMemberCommand implements Command {
 	private static final long serialVersionUID = -5348191495602297087L;
 
+    @NotNull
+    public Integer id;
+
+    @NotNull
+    public String type;
 }
