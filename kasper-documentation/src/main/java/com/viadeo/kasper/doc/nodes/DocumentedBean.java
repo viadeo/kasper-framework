9b32605fe9a81f1f1f3a34311ae13a520e838020
cmurer <cmurer@viadeoteam.com>
2014-04-08 12:16:11 +0200

    fix problem around a list of LinkedConcept

--- kasper-documentation/src/main/java/com/viadeo/kasper/doc/nodes/DocumentedBean.java
+++ kasper-documentation/src/main/java/com/viadeo/kasper/doc/nodes/DocumentedBean.java
@@ -18,10 +18,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.persistence.Transient;
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
+import java.lang.reflect.*;
 import java.util.*;
 
 /*
@@ -248,48 +245,77 @@ public class DocumentedBean extends ArrayList<DocumentedProperty> {
 
         @Override
         public  Optional<DocumentedProperty> doExtract(final Field field, final Class clazz) {
-            final Class<?> type;
 
-            @SuppressWarnings("unchecked")
-            final Optional<Class> optType = (Optional<Class>)
-                    ReflectionGenericsResolver.getParameterTypeFromClass(
-                            field, clazz, Collection.class, 0);
+            if (field.getGenericType() instanceof ParameterizedType) {
+                final ParameterizedType genericType = (ParameterizedType) field.getGenericType();
+                final Type paramType =  genericType.getActualTypeArguments()[0];
+                final Optional<Class> optionalParamClass = ReflectionGenericsResolver.getClass(paramType);
 
-            if ( ! optType.isPresent()) {
-                LOGGER.warn(String.format(
-                        "Unable to find collection enclosed type for field %s in class %s",
-                        field.getName(), clazz.getSimpleName())
-                );
-                type = null;
-            } else {
-                type = optType.get();
-            }
+                if (optionalParamClass.isPresent()) {
+                    final Class paramClass = optionalParamClass.get();
 
-            if (LinkedConcept.class.isAssignableFrom(type)) {
-                return Optional.of(
-                        new DocumentedProperty(
+                    if( LinkedConcept.class.isAssignableFrom(paramClass)) {
+                        final Type subParamType = ((ParameterizedType) paramType).getActualTypeArguments()[0];
+                        final Class subParamClass = (Class) subParamType;
+
+                        return Optional.of(
+                                new DocumentedProperty(
+                                        field.getName(),
+                                        subParamClass.getSimpleName(),
+                                        null,
+                                        true,
+                                        true,
+                                        QueryResult.class.isAssignableFrom(subParamClass),
+                                        Sets.<DocumentedConstraint>newHashSet()
+                                )
+                        );
+                    } else {
+                        return Optional.of(
+                            new DocumentedProperty(
                                 field.getName(),
-                                type == null ? "unknown" : type.getSimpleName(),
-                                null,
-                                true,
-                                true,
-                                QueryResult.class.isAssignableFrom(type),
-                                Sets.<DocumentedConstraint>newHashSet()
-                        )
-                );
-            } else {
-                return Optional.of(
-                        new DocumentedProperty(
-                                field.getName(),
-                                type == null ? "unknown" : type.getSimpleName(),
+                                paramClass.getSimpleName(),
                                 null,
                                 true,
                                 false,
-                                QueryResult.class.isAssignableFrom(type),
+                                QueryResult.class.isAssignableFrom(paramClass),
                                 Sets.<DocumentedConstraint>newHashSet()
-                        )
-                );
+                            )
+                        );
+                    }
+                } else {
+                    @SuppressWarnings("unchecked")
+                    final Optional<Class> optType = (Optional<Class>)
+                            ReflectionGenericsResolver.getParameterTypeFromClass(field, clazz, Collection.class, 0);
+
+                    if (optType.isPresent()) {
+                        return Optional.of(
+                                new DocumentedProperty(
+                                        field.getName(),
+                                        optType.get().getSimpleName(),
+                                        null,
+                                        true,
+                                        false,
+                                        QueryResult.class.isAssignableFrom(optType.get()),
+                                        Sets.<DocumentedConstraint>newHashSet()
+                                )
+                        );
+                    } else {
+                        return Optional.of(
+                                new DocumentedProperty(
+                                        field.getName(),
+                                        "unknown",
+                                        null,
+                                        true,
+                                        false,
+                                        false,
+                                        Sets.<DocumentedConstraint>newHashSet()
+                                )
+                        );
+                    }
+                }
             }
+
+            return null;
         }
 
     }
