26a318ff7dcc266d62b10fb377e7ae1b7c18dc28
ecamper <ecamper@viadeoteam.com>
2014-06-30 16:56:02 +0200

    deleted module kasper-security-authorization

--- kasper-security-authorization/src/main/java/com/viadeo/kasper/security/authz/queries/GetGroupsQuery.java
+++
@@ -1,8 +0,0 @@
-package com.viadeo.kasper.security.authz.queries;
-
-import com.viadeo.kasper.cqrs.query.Query;
-import com.viadeo.kasper.cqrs.query.annotation.XKasperQuery;
-
-@XKasperQuery(description = "get groups for authorization")
-public class GetGroupsQuery implements Query {
-}
