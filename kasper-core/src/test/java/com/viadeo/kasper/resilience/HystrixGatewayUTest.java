6c640e2ab0ff98d92028b23bca4225c1f1fa5878
Christian Nguyen Van Than <christian.nguyenvanthan@gmail.com>
2014-07-21 18:37:06 +0200

    refactor bloated test

--- kasper-core/src/test/java/com/viadeo/kasper/resilience/HystrixGatewayUTest.java
+++ kasper-core/src/test/java/com/viadeo/kasper/resilience/HystrixGatewayUTest.java
@@ -1,28 +1,32 @@
 package com.viadeo.kasper.resilience;
 
 import com.codahale.metrics.MetricRegistry;
+import com.netflix.hystrix.strategy.HystrixPlugins;
+import com.netflix.hystrix.strategy.metrics.HystrixMetricsPublisher;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
 
 public class HystrixGatewayUTest {
 
+
     @Test
-    public void testRegisterMetrics_should_not_throws_exception() throws Exception {
+    public void testRegisterMetrics_should_register_only_once_and_without_exception() throws Exception {
 
-        // nothing is initialized
+        // reset flag
         HystrixGateway.metricNotInitialized = true;
-        HystrixGateway.registerMetricPlugin(null);
-        assertTrue(HystrixGateway.metricNotInitialized);
-
         MetricRegistry metricRegistry = new MetricRegistry();
+        HystrixPlugins hystrixPlugins = spy(HystrixPlugins.getInstance());
 
         // initialized a metric registry for kasper metric
-        HystrixGateway.registerMetricPlugin(metricRegistry);
-        assertFalse(HystrixGateway.metricNotInitialized);
-
+        HystrixGateway.registerMetricPlugin(metricRegistry, hystrixPlugins);
         // try to register the metrics a second time, hystrix throws an IllegalStateException in this case
-        HystrixGateway.registerMetricPlugin(metricRegistry);
+        HystrixGateway.registerMetricPlugin(metricRegistry, hystrixPlugins);
+
+        verify(hystrixPlugins, atMost(1)).registerMetricsPublisher(any(HystrixMetricsPublisher.class));
+
     }
 }
