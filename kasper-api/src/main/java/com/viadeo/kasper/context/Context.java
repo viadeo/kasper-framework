60a1e6eb351823cdaa1d39a9a2fc2661d6966198
cmurer <cmurer@viadeoteam.com>
2015-04-27 12:34:51 +0200

    apply review remarks

--- kasper-api/src/main/java/com/viadeo/kasper/context/Context.java
+++ kasper-api/src/main/java/com/viadeo/kasper/context/Context.java
@@ -1,3 +1,9 @@
+// ============================================================================
+//                 KASPER - Kasper is the treasure keeper
+//    www.viadeo.com - mobile.viadeo.com - api.viadeo.com - dev.viadeo.com
+//
+//           Viadeo Framework for effective CQRS/DDD architecture
+// ============================================================================
 package com.viadeo.kasper.context;
 
 import com.google.common.base.Joiner;
