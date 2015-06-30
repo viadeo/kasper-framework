f77a86ecde87cf68d74ef63a46b00f0acdf31474
cmurer <cmurer@viadeoteam.com>
2015-06-30 15:00:34 +0200

    [saga] add javadoc

--- kasper-core/src/main/java/com/viadeo/kasper/event/annotation/XKasperSaga.java
+++ kasper-core/src/main/java/com/viadeo/kasper/event/annotation/XKasperSaga.java
@@ -15,9 +15,8 @@ import java.lang.annotation.Target;
 import java.util.concurrent.TimeUnit;
 
 /**
- *
- * Kasper Saga marker
- *
+ *  The <code>XKasperSaga</code> annotation specifies some meta information of a saga.
+ *  In more it provide a set of annotations allowing to define a <code>Saga</code>.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
@@ -34,54 +33,98 @@ public @interface XKasperSaga {
     Class<? extends Domain> domain();
 
 
+    // ------------------------------------------------------------------------
+
+    /**
+     * The <code>Start</code> annotation is used to declare a start step into the life cycle of a saga.
+     */
     @Retention(RetentionPolicy.RUNTIME)
     @Target(ElementType.METHOD)
     public @interface Start {
 
         /**
-         * @return the kasper event's identifier getter (in order to find the saga instance)
+         * Indicate the method name of the event for which we get the saga identifier.
+         * @return the saga identifier
          */
         String getter();
 
     }
 
+    // ------------------------------------------------------------------------
+
+    /**
+     * The <code>End</code> annotation is used to declare an end step into the life cycle of a saga.
+     */
     @Retention(RetentionPolicy.RUNTIME)
     @Target(ElementType.METHOD)
     public @interface End {
 
         /**
-         * @return the kasper event's identifier getter (in order to find the saga instance)
+         * Indicate the method name of the event for which we get the saga identifier.
+         * @return the saga identifier
          */
         String getter();
 
     }
 
+    // ------------------------------------------------------------------------
+
+    /**
+     * The <code>Step</code> annotation is used to declare a basic step.
+     * This step has no impact on the life cycle of a saga.
+     */
     @Retention(RetentionPolicy.RUNTIME)
     @Target(ElementType.METHOD)
     public @interface Step {
 
         /**
-         * @return the kasper event's identifier getter (in order to find the saga instance)
+         * Indicate the method name of the event for which we get the saga identifier.
+         * @return the saga identifier
          */
         String getter();
 
     }
 
+    // ------------------------------------------------------------------------
+
+    /**
+     * The <code>Schedule</code> annotation is used to schedule a method invocation when the related step is triggered.
+     * The annotation must be used in addition to the following annotations : <code>Start</code>, <code>Step</code>. and <code>End</code>.
+     */
     @Retention(RetentionPolicy.RUNTIME)
     @Target(ElementType.METHOD)
     public @interface Schedule {
 
+        /**
+         * @return the delay to invoke the method
+         */
         long delay();
 
+        /**
+         *
+         * @return the unit of the delay
+         */
         TimeUnit unit();
 
+        /**
+         * @return the method name to be invoked
+         */
         String methodName();
     }
 
+    // ------------------------------------------------------------------------
+
+    /**
+     * The <code>CancelSchedule</code> annotation is used to schedule a method invocation when the related step is triggered.
+     * The annotation must be used in addition to the following annotations : <code>Start</code>, <code>Step</code>. and <code>End</code>.
+     */
     @Retention(RetentionPolicy.RUNTIME)
     @Target(ElementType.METHOD)
     public @interface CancelSchedule {
 
+        /**
+         * @return the method name for which we have a scheduled invocation
+         */
         String methodName();
     }
 
