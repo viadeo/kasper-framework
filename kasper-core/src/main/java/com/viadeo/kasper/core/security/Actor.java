9cc1ad180210e9022811e66f59fa0cda8dc38670
cmurer <cmurer@viadeoteam.com>
2015-08-06 16:20:59 +0200

    move authorization part from platform

---
+++ kasper-core/src/main/java/com/viadeo/kasper/core/security/Actor.java
@@ -0,0 +1,86 @@
+// ============================================================================
+//                 KASPER - Kasper is the treasure keeper
+//    www.viadeo.com - mobile.viadeo.com - api.viadeo.com - dev.viadeo.com
+//
+//           Viadeo Framework for effective CQRS/DDD architecture
+// ============================================================================
+package com.viadeo.kasper.core.security;
+
+import com.google.common.collect.Sets;
+import com.viadeo.kasper.api.id.ID;
+
+import java.util.Collection;
+import java.util.Set;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class Actor {
+
+    private final ID actorID;
+    private Set<Role> roles;
+    private Set<Permission> permissions;
+
+    // ------------------------------------------------------------------------
+    public Actor(final ID actorID) {
+        this.actorID = checkNotNull(actorID);
+        this.roles = Sets.newHashSet();
+        this.permissions = Sets.newHashSet();
+    }
+
+    // -----------------------------------------------------------------------
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public Set<Permission> getPermissions() {
+        return permissions;
+    }
+
+    // ------------------------------------------------------------------------
+
+    public void setRoles(final Set<Role> roles) {
+        this.roles = checkNotNull(roles);
+    }
+
+    public void addRoles(final Set<Role> roles) {
+        this.roles.addAll(checkNotNull(roles));
+    }
+
+    public void setPermissions(final Set<Permission> permissions) {
+        this.permissions = checkNotNull(permissions);
+    }
+
+    public void addPermissions(final Collection<Permission> permissions) {
+        this.permissions.addAll(checkNotNull(permissions));
+    }
+
+    public void removePermissions(final Collection<Permission> permissions) {
+        this.permissions.removeAll(checkNotNull(permissions));
+    }
+
+    // ------------------------------------------------------------------------
+
+    public boolean isPermitted(final Permission p) {
+        checkNotNull(p);
+        if ((null != this.permissions) && (!this.permissions.isEmpty())) {
+            for (final Permission perm : this.permissions) {
+                if (perm.implies(p)) {
+                    return true;
+                }
+            }
+        }
+        if ((null != this.roles) && (!this.roles.isEmpty())) {
+            for (final Role role : this.roles) {
+                if (role.isPermitted(p)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean hasRole(final Role role) {
+        return this.roles.contains(checkNotNull(role));
+    }
+}
+
---
+++ kasper-core/src/main/java/com/viadeo/kasper/core/security/AuthorizationManager.java
@@ -0,0 +1,30 @@
+// ============================================================================
+//                 KASPER - Kasper is the treasure keeper
+//    www.viadeo.com - mobile.viadeo.com - api.viadeo.com - dev.viadeo.com
+//
+//           Viadeo Framework for effective CQRS/DDD architecture
+// ============================================================================
+package com.viadeo.kasper.core.security;
+
+import com.google.common.base.Optional;
+import com.viadeo.kasper.api.id.ID;
+
+/**
+ * An AuthorizationManager performs authorization (access control) operations for any given actor.
+ * Each functions need the authorizations lists coming from {@link com.viadeo.kasper.core.component.annotation.XKasperAuthz.RequiresPermissions} or
+ * {@link com.viadeo.kasper.core.component.annotation.XKasperAuthz.RequiresRoles} annotations, a {@link CombinesWith} specifying
+ * the operation (default "AND"), the current actor and an optional targetId (to which the permission holder) indicated by the
+ * {@link com.viadeo.kasper.api.annotation.XKasperAuthz.TargetId} annotation.
+ *
+ * @see com.viadeo.kasper.core.component.annotation.XKasperAuthz.RequiresPermissions
+ * @see com.viadeo.kasper.core.component.annotation.XKasperAuthz.RequiresRoles
+ * @see CombinesWith
+ * @see com.viadeo.kasper.api.annotation.XKasperAuthz.TargetId
+ */
+public interface AuthorizationManager {
+
+    boolean isPermitted(String[] permissions, CombinesWith combinesWith, ID actorID, Optional targetId);
+
+    boolean hasRole(String[] roles, CombinesWith combinesWith, ID actorID, Optional targetId);
+
+}
